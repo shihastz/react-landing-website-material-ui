799ea3eb50b33be7b5f94e0574999285e524af5c
shihastz <shihast0503@gmail.com>
2021-06-04 20:01:21 +0530

    Updates

---
+++ precache-manifest.d36814c99e995348b83049cd5967d2aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2dd801fa02e8411442d4173c5c27055",
+    "url": "/react-landing-website-material-ui/index.html"
+  },
+  {
+    "revision": "32b53c4551308b5d6c8b",
+    "url": "/react-landing-website-material-ui/static/css/main.a0311215.chunk.css"
+  },
+  {
+    "revision": "c9b3519f8f4052a48363",
+    "url": "/react-landing-website-material-ui/static/js/2.8bebc64f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-landing-website-material-ui/static/js/2.8bebc64f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b53c4551308b5d6c8b",
+    "url": "/react-landing-website-material-ui/static/js/main.c68ccf7b.chunk.js"
+  },
+  {
+    "revision": "4de278ade2695f6dfa65",
+    "url": "/react-landing-website-material-ui/static/js/runtime-main.15328ea1.js"
+  }
+]);
